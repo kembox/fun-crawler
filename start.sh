2fd0ccf79b1c12c6025f6c171ae737fabdd47dac
t13ag <tgnuadm@outlook.com>
2023-11-15 18:59:50 +0700

    change dir, update input list

--- start.sh
+++ start.sh
@@ -5,13 +5,14 @@
 
 domain="$1"
 
-ts=$(date +%Y%m%d%H%M)
-base_dir="/tmp/fun_crawler_${ts}"
+ts=$(date +%Y%m%d%H)
+base_dir="/var/tmp/fun-crawler/${domain}/${ts}"
 log_file=${base_dir}/logs.txt
 input_dir=${base_dir}/input
 results_output=${base_dir}/results.txt
 mkdir -p $base_dir
 mkdir -p $input_dir
+
 
 function log() {
     echo $(date) "$@" | tee ${log_file}
@@ -19,11 +20,11 @@ function log() {
 
 function wayback() {
     domain="$1"
-    waybackurls -dates -no-subs ${domain} | fgrep '2023-11-' | egrep "html?$" | awk '{print $2}' | sort -n | uniq  > $input_dir/wayback.list
+    waybackurls -dates -no-subs ${domain} | fgrep '2023-11-' | egrep "html?$" | awk '{print $2}' | sort -n | uniq  > $input_dir/${domain}_wayback.list
 }
 function by_hakrawler() {
     domain="$1"
-    echo "https://${domain}" | hakrawler  | grep "https://${domain}" | egrep "html?$"  | awk '{print $2}' | sort -n | uniq > $input_dir/hakrawler.list
+    echo "https://${domain}" | hakrawler  | grep "https://${domain}" | egrep "html?$"  | awk '{print $2}' | sort -n | uniq > $input_dir/${domain}_hakrawler.list
 
 }
 
