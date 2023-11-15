2e2766207b583562ac5c519e6169d6d2931aed73
t13bag <tgnuadm@outlook.com>
2023-11-15 13:10:38 +0700

    add dotnet webapp

---
+++ dotnet-webapp/top10/Pages/Report.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace top10.Pages;
+
+public class ReportModel: PageModel
+{
+    private readonly ILogger<ReportModel> _logger;
+
+    public ReportModel(ILogger<ReportModel> logger)
+    {
+        _logger = logger;
+    }
+    public void OnGet()
+    {
+    }
+
+}
